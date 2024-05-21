afe20b954b0bbe17457dd1854d65dfd3e31987b0
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2024-05-21 16:03:33 +0200

    docs

--- src/parallel_elementwise_multiplication_dynamic.v
+++ src/parallel_elementwise_multiplication_dynamic.v
@@ -3,6 +3,7 @@
 module parallel_elementwise_multiplication_dynamic #(parameter N = 8, parameter M = 2) (
     // N: Bits in integer
     // M: Number of modules to generate
+    //    Corresponds to the number of arrays to be multiplied together (must be even).
 
     input wire [N-1:0] a [0:M-1][0:N-1],
     input wire [N-1:0] b [0:M-1][0:N-1],
